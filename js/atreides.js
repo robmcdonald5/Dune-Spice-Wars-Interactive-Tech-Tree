33e06cb23927d84c31f2c4eb6a91bb69b3eb5d77
robmcdonald5 <77365455+robmcdonald5@users.noreply.github.com>
2024-06-02 17:35:27 -0500

    Updated Faction Design and Popup menu(s)

--- js/atreides.js
+++ js/atreides.js
@@ -1,2 +1,14 @@
-// This file will remain empty for now since we 
-// are not yet dynamically loading content. 
+function closeOverlay(overlayId) {
+    document.getElementById(overlayId).style.display = "none";
+}
+
+// Event listener for all navigation links
+const navLinks = document.querySelectorAll("#atreides-navigation a");
+navLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault(); // Prevent default link behavior
+
+        const targetOverlayId = link.getAttribute("data-target");
+        document.getElementById(targetOverlayId).style.display = "block";
+    });
+});
