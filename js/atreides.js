7797b805dd01d133394bd7d707b62ed2bcc642c3
robmcdonald5 <77365455+robmcdonald5@users.noreply.github.com>
2024-06-03 18:55:17 -0500

    Frmt

--- js/atreides.js
+++ js/atreides.js
@@ -9,17 +9,13 @@ navLinks.forEach(link => {
         event.preventDefault(); // Prevent default link behavior
 
         const targetOverlayId = link.getAttribute("data-target");
-        const overlayContent = document.getElementById(targetOverlayId).querySelector('.overlay-content'); 
+        const overlayContent = document.getElementById(targetOverlayId).querySelector('.overlay-content');
 
         // Fetch content from the href and inject it into the overlay
         fetch(link.getAttribute("href"))
             .then(response => response.text())
             .then(html => {
-                // Find the h2 tag in the overlay content 
-                const h2 = overlayContent.querySelector('h2'); 
-
-                // Insert the fetched HTML after the h2
-                h2.insertAdjacentHTML('afterend', html); 
+                overlayContent.innerHTML = html; // Update overlay content
 
                 // After content is loaded, re-attach event listeners for tooltips
                 const techNodes = document.querySelectorAll('.tech-node');
@@ -33,7 +29,6 @@ navLinks.forEach(link => {
     });
 });
 
-// Function to show tooltip 
 function showTooltip(event) {
     const node = event.target;
     const info = node.getAttribute('data-tech-info');
